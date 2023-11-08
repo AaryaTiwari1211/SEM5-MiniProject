c9b6b9585033ffc30ab18be9083b49ee4f5e3f73
AaryaTiwari1211 <aarya.tiwari@somaiya.edu>
2023-11-08 15:01:14 +0530

    Made the Job Portal

--- homeland/admin-panel/admins/logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-   require "../layouts/header.php";
-    session_unset();
-    session_destroy();
-
-    echo "<script>window.location.href='".ADMINURL."/admins/login-admins.php' </script>";
